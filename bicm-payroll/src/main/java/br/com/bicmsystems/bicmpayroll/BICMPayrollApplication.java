8e30b17c0cf8fe82553048793219417c4f8cc7e8
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-21 09:24:26 -0300

    Project bicm-payroll created

---
+++ bicm-payroll/src/main/java/br/com/bicmsystems/bicmpayroll/BICMPayrollApplication.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BICMPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BICMPayrollApplication.class, args);
+	}
+
+}
---
+++ bicm-payroll/src/test/java/br/com/bicmsystems/bicmpayroll/BICMPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BICMPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
