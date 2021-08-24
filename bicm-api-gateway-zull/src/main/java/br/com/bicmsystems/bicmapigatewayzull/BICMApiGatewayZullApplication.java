afba873d3dcf7aef8e0e47fa43ea52d94b9824c1
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-24 06:49:00 -0300

    API Gateway Zuul created

---
+++ bicm-api-gateway-zull/src/main/java/br/com/bicmsystems/bicmapigatewayzull/BICMApiGatewayZullApplication.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmapigatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BICMApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BICMApiGatewayZullApplication.class, args);
+	}
+
+}
