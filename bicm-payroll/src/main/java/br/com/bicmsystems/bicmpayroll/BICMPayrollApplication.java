3784601de196d3111bb1c4255307f8d112c1ccfe
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-24 06:32:15 -0300

    Configuring Eureka Clients

--- bicm-payroll/src/main/java/br/com/bicmsystems/bicmpayroll/BICMPayrollApplication.java
+++ bicm-payroll/src/main/java/br/com/bicmsystems/bicmpayroll/BICMPayrollApplication.java
@@ -3,9 +3,11 @@ package br.com.bicmsystems.bicmpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @RibbonClient(name = "bicm-worker")
 @EnableFeignClients
 @SpringBootApplication
