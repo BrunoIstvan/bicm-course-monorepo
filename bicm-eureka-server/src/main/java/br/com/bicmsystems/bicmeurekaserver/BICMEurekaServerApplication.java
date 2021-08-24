fb282e68a94fa15cd709609d99130a2bf45879a1
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-24 06:24:22 -0300

    Configuring Eureka Server

--- bicm-eureka-server/src/main/java/br/com/bicmsystems/bicmeurekaserver/BICMEurekaServerApplication.java
+++ bicm-eureka-server/src/main/java/br/com/bicmsystems/bicmeurekaserver/BICMEurekaServerApplication.java
@@ -2,12 +2,14 @@ package br.com.bicmsystems.bicmeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class BicmEurekaServerApplication {
+public class BICMEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BicmEurekaServerApplication.class, args);
+		SpringApplication.run(BICMEurekaServerApplication.class, args);
 	}
 
 }
