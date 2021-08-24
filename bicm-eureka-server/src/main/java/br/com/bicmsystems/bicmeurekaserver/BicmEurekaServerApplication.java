b5740be9e3ae6be9717b590c838ccfbadaab0cfa
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-23 22:45:27 -0300

    eureka server project created

---
+++ bicm-eureka-server/src/main/java/br/com/bicmsystems/bicmeurekaserver/BicmEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicmEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicmEurekaServerApplication.class, args);
+	}
+
+}
---
+++ bicm-eureka-server/src/test/java/br/com/bicmsystems/bicmeurekaserver/BicmEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicmEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
