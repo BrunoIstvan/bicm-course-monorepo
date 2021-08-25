340ac3146d0ce768e293a3c29dc047079a1ee079
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-25 06:39:38 -0300

    Hystrix configuration into Payroll API

--- bicm-payroll/src/main/java/br/com/bicmsystems/bicmpayroll/controllers/PaymentController.java
+++ bicm-payroll/src/main/java/br/com/bicmsystems/bicmpayroll/controllers/PaymentController.java
@@ -1,8 +1,11 @@
 package br.com.bicmsystems.bicmpayroll.controllers;
 
+import br.com.bicmsystems.bicmpayroll.entities.PaymentModel;
 import br.com.bicmsystems.bicmpayroll.exceptions.WorkerNotFoundException;
 import br.com.bicmsystems.bicmpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,7 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<?> getPayment(@PathVariable Long workerId,
                                         @PathVariable Integer days)
@@ -23,6 +27,11 @@ public class PaymentController {
         if(workerId < 0 || days < 0)
             return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(service.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<?> getPaymentAlternative(Long workerId, Integer days) {
+        return new ResponseEntity("Infelizmente estamos com algum problema e já estamos atuando nisso",
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
