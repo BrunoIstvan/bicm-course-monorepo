ffa00dac46c7e0da0a6aa3d43f3ee9e372150072
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-26 07:16:26 -0300

    API User created

---
+++ bicm-user/src/main/java/br/com/bicmsystems/bicmuser/BicmUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicmUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicmUserApplication.class, args);
+	}
+
+}
---
+++ bicm-user/src/test/java/br/com/bicmsystems/bicmuser/BicmUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicmUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
