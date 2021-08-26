1fcca2224616f27eeda92d51a3add5754e970283
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-26 08:30:28 -0300

    OAuth Application Server created

---
+++ bicm-oauth/src/main/java/br/com/bicmsystems/bicmoauth/BICMOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BICMOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BICMOauthApplication.class, args);
+	}
+
+}
---
+++ bicm-oauth/src/test/java/br/com/bicmsystems/bicmoauth/BicmOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicmOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
