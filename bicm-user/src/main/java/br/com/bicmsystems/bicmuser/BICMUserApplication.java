71b9e0637dd08093f2ffe1920a9107b911272b7d
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-26 07:48:04 -0300

    User, role, many to many association

--- bicm-user/src/main/java/br/com/bicmsystems/bicmuser/BICMUserApplication.java
+++ bicm-user/src/main/java/br/com/bicmsystems/bicmuser/BICMUserApplication.java
@@ -2,12 +2,14 @@ package br.com.bicmsystems.bicmuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class BicmUserApplication {
+public class BICMUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BicmUserApplication.class, args);
+		SpringApplication.run(BICMUserApplication.class, args);
 	}
 
 }
---
+++ bicm-user/src/main/java/br/com/bicmsystems/bicmuser/entities/UserModel.java
@@ -0,0 +1,55 @@
+package br.com.bicmsystems.bicmuser.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class UserModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleModel> roles = new HashSet();
+
+    public void setRoles(Set<RoleModel> roles) throws IllegalAccessException {
+        throw new IllegalAccessException("This field accepts only get method");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return email.equals(userModel.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+}
