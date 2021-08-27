fc9b83469ea44911800e4bb8748d28b464664ae1
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-08-27 07:12:29 -0300

    rename zuul Starter Classes into project

---
+++ bicm-api-gateway-zuul/src/test/java/br/com/bicmsystems/bicmapigatewayzuul/BICMApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bicmsystems.bicmapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BICMApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
